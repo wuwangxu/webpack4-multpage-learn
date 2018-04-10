fd1562e7d700b26ae05f29fbcc9a2a01d498ab73
tanjiahui <673800357@qq.com>
2018-04-10 12:48:47 +0800

    initial

---
+++ src/index/index.js
@@ -0,0 +1,3 @@
+import './index.css';
+// console.log("hello world")
+// document.body.innerHTML = "<span>xxxx</span>"
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // html引擎
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); //抽离css
+
+const config = {
+    entry: {
+        main: "./src/index/index.js",
+        other: "./src/other.js"
+    },
+    output: {
+        filename: "[name].js",
+        path: __dirname + "/dist"
+    },
+    module: {
+        rules: [{ //css-loader配置
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({ //用于css分离
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        }, ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index/index.html" }), new ExtractTextPlugin("[name].css"), new webpack.HotModuleReplacementPlugin()],
+    resolve: {
+        extensions: [".js", ".json", ".jsx", ".css"]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8000,
+        hot: true
+    }
+};
+module.exports = config;
