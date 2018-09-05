8374950f50c863c0100b66e24c8ac4d9fd1f991b
jiahuitan <jiahuitan@tencent.com>
2018-09-05 19:21:51 +0800

    配置格式化

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,18 +1,24 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // html引擎
-//const ExtractTextPlugin = require('extract-text-webpack-plugin'); //抽离css
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const base = require("./webpack.base.config");
+const webpack = require('webpack');
+const path = require('path');
+const base = require('./webpack.base.config');
 base.devServer = {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 8000,
-    hot: true
+	contentBase: path.join(__dirname, 'dist'),
+	compress: true,
+	port: 8000,
+	hot: true
 };
-base.devtool = "inline-source-map";
+base.devtool = 'inline-source-map';
+base.module.rules.push({
+	test: /\.css$/,
+	use: [
+		'css-hot-loader', //支持热更新
+		MiniCssExtractPlugin.loader,
+		'css-loader',
+		'postcss-loader'
+	]
+});
 base.plugins.push(new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin());
 const config = {
-    ...base
+	...base
 };
 module.exports = config;
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,10 +1,10 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 //const ExtractTextPlugin = require('extract-text-webpack-plugin'); //抽离css
-const base = require("./webpack.base.config");
+const base = require('./webpack.base.config');
 base.plugins.push(new UglifyJSPlugin()); //webpack4 该source map默认false
 const config = {
-    ...base
+	...base
 };
 module.exports = config;
